372cac077e5a7df56a089e393429634bba943bd9
x4nemi <ximena.garcia7482@alumnos.udg.mx>
2024-05-04 21:39:19 -0600

    Componente para cambiar tema oscuro y claro de la app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { CssBaseline, CssVarsProvider, Grid, extendTheme } from '@mui/joy'
 
+const theme = extendTheme({ cssVarPrefix: 'demo'})
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<CssVarsProvider
+			defaultMode='dark'
+			theme={theme}
+			disableNestedContext
+		>
+			<CssBaseline />
+			<Grid container justifyContent="center" alignItems="center" sx={{ height: '100vh' }}>
+				<App />
+			</Grid>
+		</CssVarsProvider>
+	</React.StrictMode>
 )
