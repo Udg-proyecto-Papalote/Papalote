368b3521657241fafb80fbba6b18cb9bc5298db0
x4nemi <ximena.garcia7482@alumnos.udg.mx>
2024-05-08 21:19:21 -0600

    Rutas básicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { Grid } from '@mui/joy'
 import { ExtendedNavBar } from './components/ExtendedNavBar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import { Inicio } from './pages/Inicio'
 import { Diagnostico } from './pages/Diagnostico'
 import { Ejercicios } from './pages/Ejercicios'
+import { InicioSesion } from './pages/InicioSesion'
+import { Registro } from './pages/Registro'
 
 function App() {
+	const { pathname } = useLocation()
 	return (
 		<Grid width='100%'>
-			<ExtendedNavBar />
+			{
+				pathname !== '/iniciarsesion' && pathname !== '/registro' && <ExtendedNavBar />
+			}
 			<Grid>
 				<Routes>
-					<Route path='/' element={<Inicio />} />
+					<Route path='*' element={<Inicio />} />
 					<Route path='/ejercicios' element={<Ejercicios />} />
 					<Route path='/diagnostico' element={<Diagnostico />} />
+					<Route path='/iniciarsesion' element={<InicioSesion />} />
+					<Route path='/registro' element={<Registro />} />
 				</Routes>
 			</Grid>
 		</Grid>
