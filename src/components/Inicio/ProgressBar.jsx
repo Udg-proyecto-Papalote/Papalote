83b8ce187f7099a956d9f3e4112e7973291c49a1
x4nemi <ximena.garcia7482@alumnos.udg.mx>
2024-05-28 23:48:08 -0600

    Agregué porcentaje de ejercicios realizados

--- src/components/Inicio/ProgressBar.jsx
+++ src/components/Inicio/ProgressBar.jsx
@@ -1,16 +1,17 @@
 import { Box, Card, CardContent, CircularProgress, Grid, Typography } from "@mui/joy"
+import PropTypes from "prop-types";
 
-export const ProgressBar = () => {
+export const ProgressBar = ({ percentage = 20 }) => {
 	return (
 		<Grid md={2}>
 
 			<Card>
 				<CardContent>
-					<CircularProgress sx={{ '--CircularProgress-size': '140px', '--CircularProgress-thickness': '12px' }} determinate value={20}>
+					<CircularProgress sx={{ '--CircularProgress-size': '140px', '--CircularProgress-thickness': '12px' }} determinate value={ percentage }>
 						<Box
 							sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 0 }}
 						>
-							<Typography level="h4">20%</Typography>
+							<Typography level="h3">20%</Typography>
 							<Typography level="body-sm">Ejercicios</Typography>
 							<Typography level="body-sm">realizados</Typography>
 
@@ -20,4 +21,9 @@ export const ProgressBar = () => {
 			</Card>
 		</Grid>
 	)
+}
+
+// Props
+ProgressBar.propTypes = {
+	percentage: PropTypes.number
 }
