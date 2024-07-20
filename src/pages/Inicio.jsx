8ec9b9da4c228a4501ad6ae24045bace43c56b7a
Ximena <ximena.garcia7482@alumnos.udg.mx>
2024-07-20 01:07:43 -0600

    Inicio widgets

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,9 +1,17 @@
 import { Grid, Typography } from "@mui/joy"
+import { StreakDays } from "../components/Inicio/StreakDays"
+import { ProgressBar } from "../components/Inicio/ProgressBar"
+import { StartDiagnostic } from "../components/Inicio/StartDiagnostic"
 
 export const Inicio = () => {
     return (
-        <Grid>
-            <Typography level='h1'>Inicio</Typography>
+        <Grid mx={10}>
+            {/* <Typography level='h1'>Inicio</Typography> */}
+            <Grid container gap={2}>
+                <StreakDays />
+                <ProgressBar percentage={20} />
+                <StartDiagnostic isFirstTime={true} />
+            </Grid>
         </Grid>
     )
 }
