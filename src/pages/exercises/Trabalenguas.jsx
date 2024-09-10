a8de09083660817cfc19b0af6e240dea8dd29d31
Ximena <ximena.garcia7482@alumnos.udg.mx>
2024-09-10 13:29:32 -0600

    Update Trabalenguas page with new exercises and dynamic rendering

--- src/pages/exercises/Trabalenguas.jsx
+++ src/pages/exercises/Trabalenguas.jsx
@@ -1,10 +1,13 @@
 import { Info, LibraryBooks, StarRounded } from "@mui/icons-material"
 import { Alert, Card, CardContent, Chip, Grid, List, ListItem, Tab, TabList, TabPanel, Tabs, Typography, useColorScheme } from "@mui/joy"
 import trab from '../../assets/trabalenguas.json'
+import { exercises } from "./data"
+import { useState } from "react"
 
-export const Trabalenguas = ({ title = 'Trabalenguas', theme = 'Dicción', instructions = 'Pronuncia en voz alta el siguiente trabalenguas con claridad y precisión. Hazlo a un ritmo moderado, asegurándote de articular correctamente cada palabra.', content = trab }) => {
-    const { mode } = useColorScheme()
-    const { tigres } = trab
+export const Trabalenguas = ({ exerciseName = 'Diccion1' }) => {
+    const { title, theme, instructions, recommendations, trabalenguas } = exercises[exerciseName]
+    const [trabalengua, setTrabalengua] = useState(0)
+    const mode = useColorScheme()
 
     const handleMouseOver = (e) => {
         e.target.style.backgroundColor = mode === 'dark' ? '#4d7c0f' : '#a3e635'
@@ -30,7 +33,7 @@ export const Trabalenguas = ({ title = 'Trabalenguas', theme = 'Dicción', instr
                 sx={{
                     alignItems: 'center',
                 }}>
-                <Typography level='h1'>{title}</Typography>
+                <Typography level='h2'>{title}</Typography>
                 <Chip variant="soft" color="primary" size="lg" sx={{ mt: 1 }}>{theme.toUpperCase()}</Chip>
             </Grid>
             <Tabs aria-label="tabs" defaultValue={0} sx={{ bgcolor: 'transparent' }}>
@@ -42,20 +45,27 @@ export const Trabalenguas = ({ title = 'Trabalenguas', theme = 'Dicción', instr
                         borderRadius: '10px'
                     }}
                 >
-                    <Tab disableIndicator variant="outlined" color='primary'>Info</Tab>
+                    <Tab disableIndicator variant="outlined" color='primary'>Instrucciones</Tab>
                     <Tab disableIndicator variant="outlined" color='success'>Recomendaciones</Tab>
                 </TabList>
                 <TabPanel value={0} sx={{ px: 0, pt: 0.4, pb: 1 }}>
-                    <Alert variant='soft' startDecorator={<Info fontSize='30' />} color='primary' size='lg'>
+                    <Alert variant='soft' color='primary' size='lg'>
                         <div>
-                            <span style={{ fontSize: 20 }}>Instrucciones</span>
-                            <List marker="circle">
-                                <ListItem sx={{py:0}}>
-                                    <Typography level="body-md" >
-                                        {instructions}
-                                    </Typography>
-                                </ListItem>
-                                <ListItem sx={{py:0}}>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <Info fontSize='30' sx={{ mr: 1 }} />
+                                <span style={{ fontSize: 20 }}>Instrucciones</span>
+                            </div>
+                            <List marker="circle" sx={{ ml: 3 }}>
+                                {
+                                    instructions.split('.').map((instruction, index) => (
+                                        instruction.length > 0 && <ListItem key={index}>
+                                            <Typography level="body-md" >
+                                                {instruction + '.'}
+                                            </Typography>
+                                        </ListItem>
+                                    ))
+                                }
+                                <ListItem sx={{ py: 0 }}>
                                     <Typography level="body-md" >
                                         Pasa tu mouse sobre la oración para enfatizarla.
                                     </Typography>
@@ -65,12 +75,23 @@ export const Trabalenguas = ({ title = 'Trabalenguas', theme = 'Dicción', instr
                     </Alert>
                 </TabPanel>
                 <TabPanel value={1} sx={{ px: 0, pt: 0.4, pb: 1 }}>
-                    <Alert variant='soft' startDecorator={<StarRounded fontSize='100' />} color='success' size='lg'>
+                    <Alert variant='soft' color='success' size='lg'>
                         <div>
-                            <span style={{ fontSize: 20 }}>Recomendaciones</span>
-                            <Typography level="body-md" >
-                                Recuerda, la práctica constante es clave para el dominio. Cuanto más practiques, más mejorarás tu <Typography fontWeight='900'>dicción</Typography>. Avanza al siguiente nivel solo cuando te sientas completamente seguro de haber realizado este ejercicio con precisión y fluidez.
-                            </Typography>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <StarRounded fontSize='30' sx={{ mr: 1 }} />
+                                <span style={{ fontSize: 20 }}>Recomendaciones</span>
+                            </div>
+                            <List marker="circle" sx={{ ml: 3 }}>
+                                {
+                                    recommendations.split('.').map((recommendation, index) => (
+                                        recommendation.length > 0 && <ListItem key={index}>
+                                            <Typography level={{lg: 'body-md', sm: 'body-xs'}} >
+                                                {recommendation + '.'}
+                                            </Typography>
+                                        </ListItem>
+                                    ))
+                                }
+                            </List>
                         </div>
                     </Alert>
                 </TabPanel>
@@ -85,11 +106,13 @@ export const Trabalenguas = ({ title = 'Trabalenguas', theme = 'Dicción', instr
                 </CardContent>
                 <CardContent sx={{ py: 2, px: 5 }}>
                     <Typography textAlign='center'>
-                        {tigres.map((sentence, index) => (
-                            <Typography fontSize={{ sm: 22, lg: 26, xs: 20, md: 24 }} letterSpacing={1} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} key={index} textAlign='justify'>
-                                {sentence + '. '}
-                            </Typography>
-                        ))}
+                        {
+                            trabalenguas[trabalengua].map((sentence, index) => (
+                                <Typography fontSize={{ sm: 22, lg: 26, xs: 20, md: 24 }} letterSpacing={1} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} key={index} textAlign='justify'>
+                                    {sentence}
+                                </Typography>
+                            ))
+                        }
                     </Typography>
                 </CardContent>
             </Card>
---
+++ src/pages/exercises/data.js
@@ -0,0 +1,15 @@
+export const exercises = {
+    'Diccion1': {
+        'title': 'Trabalenguas Populares I',
+        'theme': 'Dicción',
+        'instructions': 'Pronuncia en voz alta el siguiente trabalenguas con claridad y precisión. Hazlo a un ritmo moderado, asegurándote de articular correctamente cada palabra.',
+        'recommendations': 'Cada ejercicio puede practicarse varias veces al día y durante varios días, hasta que logres pronunciar el trabalenguas de forma correcta y fluida. Repite al menos tres veces por sesión, buscando mejorar tu pronunciación y velocidad en cada repetición.',
+        'trabalenguas': [
+            ['Parra tenía una perra. ', 'Guerra tenía una parra. ', 'La perra de Parra subió a la parra de Guerra. ', 'Guerra pegó con la porra a la perra de Parra. Y Parra le dijo a Guerra: ', '¿Por qué ha pegado Guerra con la porra a la perra de Parra? ', 'Y Guerra le contestó: ', 'Si la perra de Parra no hubiera subido a la parra de Guerra, ', 'Guerra no habría pegado con la porra a la perra de Parra.'],
+            'Pamplinoso pepe peplo: una plepa pamplónica de plomo plegó un papel; sí, pepe peplo, la pepla, es pamplónica plepa, el papel plegó de plomo, pepe peplo pamplinoso.',
+            'Paco le contó a su tío Peco que Coquito, el perro de su primo Pablito, estaba copado comiendo poquitos copitos de coco cortado. Mientras Pablito, clavaba un clavito y su tío preguntaba: ¿Qué clavito clavó Pablito cuando Coquito copado comía poquitos copitos de coco cortado?',
+            'Había una gallina ética perlética pelapelambrética peluda y pelepelembrúda que tuvo a los hijos éticos perléticos pelapelambréticos peludos y pelapelambrúdos. Si la gallina no sería ética perlética pelapelambrética peluda y pelepelembrúda sus hijos no serían éticos perléticos pelapelambréticos peludos y pelapelambrúdos.',
+
+        ]
+    }
+}
