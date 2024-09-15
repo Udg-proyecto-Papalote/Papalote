9a008463090154a62f66d13b60229639d4f721d8
Ximena <ximena.garcia7482@alumnos.udg.mx>
2024-09-14 23:52:05 -0600

    Algunos features en el ejercicio escucha y repite

--- src/components/Ejercicios/CheckboxCard.jsx
+++ src/components/Ejercicios/CheckboxCard.jsx
@@ -1,16 +1,22 @@
 import { HouseOutlined } from "@mui/icons-material"
 import { Card, CardContent, Checkbox, Grid, Typography } from "@mui/joy"
 
-const CheckboxCard = ({ icon = <HouseOutlined />, title = "Hola Mundo" }) => {
+const CheckboxCard = ({ icon = <HouseOutlined />, title = "Escucha", coso = <audio controls src={'https://res.cloudinary.com/ds8hfmrth/video/upload/v1725379369/papalote/wgx7snrtak1blwi8jqhy.webm'} style={{maxWidth: '100%', zoom: 0.8 }}/> }) => {
+    const audioLink = 'https://res.cloudinary.com/ds8hfmrth/video/upload/v1725379369/papalote/wgx7snrtak1blwi8jqhy.webm';
+    
+
     return (
-        <Grid lg={4} sm={4} md={4}>
+        <Grid lg={4} sm={4} md={4} xs={12}>
             <Card >
-                <CardContent orientation="horizontal" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                    <CardContent orientation="horizontal">
+                <CardContent orientation="horizontal" sx={{ justifyContent: 'space-between' }}>
+                    <CardContent orientation="horizontal" sx={{ gap: 1 }}>
                         {icon}
-                        <Typography level="title-md">{title}</Typography>
+                        <Typography level="title-lg">{title}</Typography>
                     </CardContent>
                     <Checkbox variant='outlined' color='neutral' />
+                </CardContent>
+                <CardContent sx={{ alignItems: 'center' }}>
+                    { coso }
                 </CardContent>
             </Card>
         </Grid>
--- src/pages/exercises/EscuchaYRepite.jsx
+++ src/pages/exercises/EscuchaYRepite.jsx
@@ -1,9 +1,10 @@
-import { Grid } from "@mui/joy"
+import { AspectRatio, Grid } from "@mui/joy"
 import Instructions from "../../components/Ejercicios/Instructions"
 import Title from "../../components/Ejercicios/Title"
 import { exercises } from "./data"
 import Ending from "../../components/Ejercicios/Ending"
 import CheckboxCard from "../../components/Ejercicios/CheckboxCard"
+import { Image } from "@mui/icons-material"
 
 const EscuchaYRepite = ({ exerciseName = 'Diccion7' }) => {
     const { title, theme, intructions, recommendations, words, ending } = exercises[exerciseName]
@@ -16,7 +17,9 @@ const EscuchaYRepite = ({ exerciseName = 'Diccion7' }) => {
 
                 <Grid container spacing={2}>
                     <CheckboxCard />
-                    <CheckboxCard />
+                    <CheckboxCard coso ={ <AspectRatio ratio={1} sx={{ width: '120px', height: '80px', mt: -3, mb: 2 }} color='none'>
+                    <img src='https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTI1bWNyeWRyejhnMWl2OHAzcHQyM2VvOWY5bm10NDQ3NGI0bXZsdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/BGkUHWRF5ZiXmkWdmO/giphy.webp' alt='Celebration' />
+                </AspectRatio> } />
                     <CheckboxCard />
                 </Grid>
             </>
