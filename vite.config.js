98a4615ae30f5dff2936e7f1ff1b788805ecb8c1
Ximena <ximena.garcia7482@alumnos.udg.mx>
2024-09-18 21:59:19 -0600

    Change base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://udg-proyecto-papalote.github.io/Papalote',
+  base: 'https://udg-proyecto-papalote.github.io',
 })
