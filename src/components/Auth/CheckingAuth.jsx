349341f67030f3a829e2f2306c4febbcd3af7570
Ximena <ximena.garcia7482@alumnos.udg.mx>
2024-11-02 15:23:15 -0600

    Cambiar loading

--- src/components/Auth/CheckingAuth.jsx
+++ src/components/Auth/CheckingAuth.jsx
@@ -5,7 +5,7 @@ export const CheckingAuth = () => {
     return (
         <Grid container justifyContent="center" alignItems="center" sx={{ height: '80vh' }} direction='column' gap={2}>
             <AspectRatio ratio={1} sx={{ width: 500, borderRadius: 20 }} >
-                <img src='https://giphy.com/stickers/hana-tulip-tulips-U7KEuJF5V8J7xegCuB' alt='loading' />
+                <img src='https://cdn.dribbble.com/users/1229287/screenshots/4343870/media/ea77976e38fc7ae888003fe067559acf.gif' alt='loading' />
             </AspectRatio>
             <Typography level='h4' align='center' textAlign='center'>
                 Cargando...
--- src/pages/LoadingReport.jsx
+++ src/pages/LoadingReport.jsx
@@ -1,18 +1,7 @@
 import { Alert, AspectRatio, Grid, Typography } from '@mui/joy'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { setDiagnosticLoading } from '../store/slices/userSlice'
 import { Warning } from '@mui/icons-material'
 
 const LoadingReport = () => {
-    // const dispatch = useDispatch()
-
-    // // when leaving the page, clear the report
-    // useEffect(() => {
-    //     return () => {
-    //         dispatch(setDiagnosticLoading(false))
-    //     }
-    // }, [dispatch])
     return (
         <Grid container justifyContent="center" alignItems="center" sx={{ height: '80vh' }} direction='column' gap={2}>
             <Typography level='h4' align='center' textAlign='center'>
